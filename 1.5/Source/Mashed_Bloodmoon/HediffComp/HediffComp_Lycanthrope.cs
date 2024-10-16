478dddce29048df9caf4af98f43d56a16c9f7ae0
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-16 15:51:50 +1300

    requirements for type defs work

---
+++ 1.5/Source/Mashed_Bloodmoon/HediffComp/HediffComp_Lycanthrope.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class HediffComp_Lycanthrope : HediffComp
+    {
+        public HediffCompProperties_Lycanthrope Props => (HediffCompProperties_Lycanthrope)props;
+
+        public LycanthropeTypeDef lycanthropeTypeDef;
+        private List<FloatMenuOption> lycanthropeTypeOptions;
+
+        /// <summary>
+        /// Creates a list of conditions for the Force new condition dev gizmo
+        /// </summary>
+        private List<FloatMenuOption> LycanthropeTypeOptions
+        {
+            get
+            {
+                lycanthropeTypeOptions = new List<FloatMenuOption>();
+
+                foreach (LycanthropeTypeDef def in DefDatabase<LycanthropeTypeDef>.AllDefs)
+                {
+                    FloatMenuOption item;
+                    AcceptanceReport allowed = def.PawnRequirementsMet(parent.pawn);
+                    if (allowed)
+                    {
+                        item = new FloatMenuOption(def.label, delegate
+                        {
+                            lycanthropeTypeDef = def;
+                        });
+                        lycanthropeTypeOptions.Add(item);
+                    }
+                    else
+                    {
+                        item = new FloatMenuOption(def.label + " (" + allowed.Reason + ")", delegate
+                        {
+                            
+                        });
+                        lycanthropeTypeOptions.Add(item);
+                    }
+                }
+                return lycanthropeTypeOptions;
+
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void CompPostMake()
+        {
+            base.CompPostMake();
+            lycanthropeTypeDef = LycanthropeTypeDefOf.Mashed_Bloodmoon_Werewolf;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override IEnumerable<Gizmo> CompGetGizmos()
+        {
+            if (DebugSettings.ShowDevGizmos)
+            {
+                yield return new Command_Action
+                {
+                    defaultLabel = "DEV: Swap lycanthrope type",
+                    action = delegate ()
+                    {
+                        FloatMenu floatMenu = new FloatMenu(LycanthropeTypeOptions);
+                        Find.WindowStack.Add(floatMenu);
+                    },
+                };
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string CompDescriptionExtra => base.CompDescriptionExtra;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void CompExposeData()
+        {
+            Scribe_Defs.Look(ref lycanthropeTypeDef, "lycanthropeTypeDef");
+        }
+    }
+}
---
+++ 1.5/Source/Mashed_Bloodmoon/HediffComp/Properties/HediffCompProperties_Lycanthrope.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class HediffCompProperties_Lycanthrope : HediffCompProperties
+    {
+        public HediffCompProperties_Lycanthrope()
+        {
+            compClass = typeof(HediffComp_Lycanthrope);
+        }
+    }
+}
