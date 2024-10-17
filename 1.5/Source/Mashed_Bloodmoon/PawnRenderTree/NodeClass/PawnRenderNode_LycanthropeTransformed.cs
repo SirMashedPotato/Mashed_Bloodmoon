5ddd84a304e0bb7efddac2ba052b094e2b1a01be
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-17 15:44:53 +1300

    bleh

--- 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/NodeClass/PawnRenderNode_LycanthropeTransformed.cs
+++ 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/NodeClass/PawnRenderNode_LycanthropeTransformed.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace Mashed_Bloodmoon
 {
-    public class PawnRenderNode_LycanthropeTransformed : PawnRenderNode_Body
+    public class PawnRenderNode_LycanthropeTransformed : PawnRenderNode
     {
         public PawnRenderNode_LycanthropeTransformed(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree)
         {
@@ -12,8 +13,8 @@ namespace Mashed_Bloodmoon
         public override Graphic GraphicFor(Pawn pawn)
         {
             LycanthropeTypeDef typeDef = CompLycanthrope(pawn).lycanthropeTypeDef;
-            return GraphicDatabase.Get<Graphic_Multi>(typeDef.graphicData.texPath, null, typeDef.graphicData.drawSize, 
-                typeDef.graphicData.color, typeDef.graphicData.colorTwo, typeDef.graphicData);
+            return GraphicDatabase.Get<Graphic_Multi>(typeDef.graphicData.texPath, ShaderDatabase.CutoutComplex, typeDef.graphicData.drawSize, 
+                typeDef.graphicData.color, typeDef.graphicData.colorTwo);
         }
 
         private HediffComp_Lycanthrope CompLycanthrope(Pawn pawn)
