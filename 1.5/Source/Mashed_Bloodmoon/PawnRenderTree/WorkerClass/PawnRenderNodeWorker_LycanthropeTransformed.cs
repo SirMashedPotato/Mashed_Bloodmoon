0ad187ab5db7bb8c3556ac5398f357bcec7d7033
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-17 16:25:56 +1300

    scaling lycanthrope textures

---
+++ 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/WorkerClass/PawnRenderNodeWorker_LycanthropeTransformed.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class PawnRenderNodeWorker_LycanthropeTransformed : PawnRenderNodeWorker
+    {
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+            return true;
+        }
+
+        public override void AppendDrawRequests(PawnRenderNode node, PawnDrawParms parms, List<PawnGraphicDrawRequest> requests)
+        {
+            base.AppendDrawRequests(node, parms, requests);
+        }
+
+        /// <summary>
+        /// Scales the werewolf texture, because otherwise it's pawn sized
+        /// </summary>
+        public override Vector3 ScaleFor(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (scaleFor == Vector3.zero)
+            {
+                Vector2 drawSize = CompLycanthrope(parms.pawn).lycanthropeTypeDef.graphicData.drawSize;
+                scaleFor = new Vector3(drawSize.x, 0, drawSize.y);
+            }
+            
+            return scaleFor;
+        }
+
+        private HediffComp_Lycanthrope CompLycanthrope(Pawn pawn)
+        {
+            if (compLycanthrope == null)
+            {
+                compLycanthrope = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Mashed_Bloodmoon_Lycanthrope).TryGetComp<HediffComp_Lycanthrope>();
+            }
+            return compLycanthrope;
+        }
+
+        private HediffComp_Lycanthrope compLycanthrope;
+        private Vector3 scaleFor = Vector3.zero;
+    }
+}
