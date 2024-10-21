aa54a68ce151253b4db0011421799b8ce3b013df
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-21 14:53:41 +1300

    LTTWorker_AddHediff

---
+++ 1.5/Source/Mashed_Bloodmoon/LycanthropeTypeTransformationWorker/LTTWorker_AddHediff.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class LTTWorker_AddHediff : LycanthropeTypeTransformationWorker
+    {
+        public override void PostTransformationBegin(Pawn pawn)
+        {
+            if (hediffDef != null)
+            {
+                pawn.health.AddHediff(hediffDef);
+            }
+        }
+
+        public override void PostTransformationEnd(Pawn pawn)
+        {
+            if (hediffDef != null)
+            {
+                Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(hediffDef);
+                if (hediff != null) 
+                { 
+                    pawn.health.RemoveHediff(hediff); 
+                }
+            }
+        }
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            if (hediffDef == null)
+            {
+                yield return "hediffDef is null";
+            }
+        }
+
+        public HediffDef hediffDef;
+    }
+}
