c755f78510d20ff984ef73bc722f0e5c30bda63c
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-23 12:32:28 +1300

    LTRWorker_Record

---
+++ 1.5/Source/Mashed_Bloodmoon/LycanthropeTypeRequirementWorker/LTRWorker_Record.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    internal class LTRWorker_Record : LycanthropeTypeRequirementWorker
+    {
+        public override AcceptanceReport PawnRequirementsMet(Pawn pawn)
+        {
+            float minCount = recordCount;
+            if (inHours)
+            {
+                recordCount *= GenDate.TicksPerHour;
+            }
+            if (pawn.records.GetValue(recordDef) >= minCount)
+            {
+                return true;
+            }
+            return "Mashed_Bloodmoon_LTR_InvalidRecord".Translate();
+        }
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            if (recordDef == null)
+            {
+                yield return "recordDef is null";
+            }
+        }
+
+        public RecordDef recordDef;
+        public float recordCount = 1f;
+        public bool inHours = false;
+    }
+}
