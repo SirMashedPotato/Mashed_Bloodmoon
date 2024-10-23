07e6be6b215a6faab95fa5e81abacbe026c38b0c
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-23 15:25:25 +1300

    PawnRenderNodeWorker_LycanthropeScaled

---
+++ 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/WorkerClass/PawnRenderNodeWorker_LycanthropeScaled.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class PawnRenderNodeWorker_LycanthropeScaled : PawnRenderNodeWorker
+    {
+        /// <summary>
+        /// Scales the werewolf texture, because otherwise it's pawn sized
+        /// </summary>
+        public override Vector3 ScaleFor(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (scaleFor == Vector3.zero)
+            {
+                Vector2 drawSize = CompLycanthrope(parms.pawn).LycanthropeTypeDef.graphicData.drawSize;
+                scaleFor = new Vector3(drawSize.x * parms.pawn.DrawSize.x, 0, drawSize.y * parms.pawn.DrawSize.y);
+            }
+            return scaleFor;
+        }
+
+        public HediffComp_Lycanthrope CompLycanthrope(Pawn pawn)
+        {
+            if (compLycanthrope == null)
+            {
+                compLycanthrope = LycanthropeUtility.GetCompLycanthrope(pawn);
+            }
+            return compLycanthrope;
+        }
+
+        private Vector3 scaleFor = Vector3.zero;
+        private HediffComp_Lycanthrope compLycanthrope;
+    }
+}
---
+++ 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/WorkerClass/PawnRenderNodeWorker_LycanthropeTransformedBody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class PawnRenderNodeWorker_LycanthropeTransformedBody : PawnRenderNodeWorker_LycanthropeScaled
+    {
+        /// <summary>
+        /// Hides the pawns head, body, clothes etc
+        /// </summary>
+        public override void AppendDrawRequests(PawnRenderNode node, PawnDrawParms parms, List<PawnGraphicDrawRequest> requests)
+        {
+            for(int i = requests.Count - 1; i >= 0; i--)
+            {
+                if (!(requests[i].node is PawnRenderNode_Lycanthrope || requests[i].node.Worker is PawnRenderNodeWorker_Overlay))
+                {
+                    requests.RemoveAt(i);
+                }
+            }
+            base.AppendDrawRequests(node, parms, requests);
+        }
+    }
+}
