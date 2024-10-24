e861c4491305534b6bd070f1e3721bfa7a60a7b5
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-25 09:55:59 +1300

    LTRWorker_Totem

---
+++ 1.5/Source/Mashed_Bloodmoon/LycanthropeTypeRequirementWorker/LTRWorker_Totem.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    internal class LTRWorker_Totem : LycanthropeTypeRequirementWorker
+    {
+        public override AcceptanceReport PawnRequirementsMet(Pawn pawn)
+        {
+            HediffComp_Lycanthrope compLycanthrope = LycanthropeUtility.GetCompLycanthrope(pawn);
+            if (compLycanthrope.usedTotemTracker.TryGetValue(totemTypeDef, out int currentUsedCount) && currentUsedCount >= usedCount)
+            {
+                return true;
+            }
+            return "Mashed_Bloodmoon_LTR_InvalidTotem".Translate();
+        }
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            if (totemTypeDef == null)
+            {
+                yield return "totemTypeDef is null";
+            }
+        }
+
+        public TotemTypeDef totemTypeDef;
+        public int usedCount = 1;
+    }
+}
