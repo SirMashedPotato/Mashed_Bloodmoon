f4c1d473e8e414461c070eab074a1ffbf628c5aa
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-10-30 15:56:30 +1300

    restless sleep

--- 1.5/Source/Mashed_Bloodmoon/Harmony/HarmonyInstance.cs
+++ 1.5/Source/Mashed_Bloodmoon/Harmony/HarmonyInstance.cs
@@ -2,6 +2,7 @@
 using Verse;
 using System.Reflection;
 using RimWorld;
+using System.Collections.Generic;
 
 namespace Mashed_Bloodmoon
 {
@@ -30,6 +31,41 @@ namespace Mashed_Bloodmoon
             {
                 __result = false;
                 cantReason = "Mashed_Bloodmoon_LycanthropeCantEquip".Translate(pawn);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [HarmonyPatch(typeof(Toils_LayDown))]
+    [HarmonyPatch("ApplyBedThoughts")]
+    public static class Toils_LayDown_ApplyBedThoughts_Patch
+    {
+        public static void Postfix(Pawn actor, Building_Bed bed)
+        {
+            Log.Message("1");
+            if (LycanthropeUtility.PawnIsLycanthrope(actor))
+            {
+                if (actor.needs.mood == null)
+                {
+                    return;
+                }
+
+                if (bed != null && bed == actor.ownership.OwnedBed && !bed.ForPrisoners)
+                {
+                    int index = 0;
+
+                    if (bed.GetRoom().Role == RoomRoleDefOf.Bedroom)
+                    {
+                        index = 1;
+                    }
+                    else if (bed.GetRoom().Role == RoomRoleDefOf.Barracks)
+                    {
+                        index = 2;
+                    }
+                    actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtMaker.MakeThought(ThoughtDefOf.Mashed_Bloodmoon_RestlessSleep, index));
+                }
             }
         }
     }
