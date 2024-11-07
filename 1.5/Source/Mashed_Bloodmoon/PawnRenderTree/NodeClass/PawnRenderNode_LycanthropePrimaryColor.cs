b142d4586334ad5678bae6984bde8d6cb598ee1b
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-11-07 14:50:16 +1300

    maybe anima gets cut

---
+++ 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/NodeClass/PawnRenderNode_LycanthropePrimaryColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class PawnRenderNode_LycanthropePrimaryColor : PawnRenderNode_Lycanthrope
+    {
+        public PawnRenderNode_LycanthropePrimaryColor(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree)
+        {
+        }
+
+        public override Color ColorFor(Pawn pawn)
+        {
+            return CompLycanthrope(pawn).primaryColour;
+        }
+    }
+}
---
+++ 1.5/Source/Mashed_Bloodmoon/PawnRenderTree/NodeClass/PawnRenderNode_LycanthropeSecondaryColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class PawnRenderNode_LycanthropeSecondaryColor : PawnRenderNode_Lycanthrope
+    {
+        public PawnRenderNode_LycanthropeSecondaryColor(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree)
+        {
+        }
+
+        public override Color ColorFor(Pawn pawn)
+        {
+            return CompLycanthrope(pawn).secondaryColour;
+        }
+    }
+}
