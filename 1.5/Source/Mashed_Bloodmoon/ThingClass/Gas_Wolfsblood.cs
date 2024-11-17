9163855de5d85250e32066c18daa4c4130047af5
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-11-17 17:28:37 +1300

    wolfsblood vessel belt

---
+++ 1.5/Source/Mashed_Bloodmoon/ThingClass/Gas_Wolfsblood.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class Gas_Wolfsblood : Gas
+    {
+        private readonly int tickInterval = 60;
+
+        public override void Tick()
+        {
+            base.Tick();
+            try
+            {
+                if (this.IsHashIntervalTick(tickInterval))
+                {
+                    HashSet<Thing> hashSet = new HashSet<Thing>(Position.GetThingList(Map));
+                    if (hashSet != null)
+                    {
+                        foreach (Thing thing in hashSet)
+                        {
+                            if (thing != null && thing is Pawn p && LycanthropeUtility.PawnIsTransformedLycanthrope(p))
+                            {
+                                Hediff bloodloss = p.health.hediffSet.GetFirstHediffOfDef(RimWorld.HediffDefOf.BloodLoss);
+                                if (bloodloss != null)
+                                {
+                                    bloodloss.Severity -= 0.01f;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+
+            }
+        }
+    }
+}
