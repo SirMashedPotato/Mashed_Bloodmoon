f44eeb0a2c0378059d8de7beb246ab750a2284a2
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-11-22 17:13:56 +1300

    slight beast form reorganise

--- 1.5/Source/Mashed_Bloodmoon/LycanthropeTypeRequirementWorker/LTRWorker_BeastHunt.cs
+++ 1.5/Source/Mashed_Bloodmoon/LycanthropeTypeRequirementWorker/LTRWorker_BeastHunt.cs
@@ -13,7 +13,7 @@ namespace Mashed_Bloodmoon
             {
                 return true;
             }
-            return "Mashed_Bloodmoon_LTR_InvalidBeastHunt".Translate(beastHuntDef);
+            return "Mashed_Bloodmoon_LTR_InvalidBeastHunt".Translate(beastHuntDef.LabelCap);
         }
 
         public override IEnumerable<string> ConfigErrors()
