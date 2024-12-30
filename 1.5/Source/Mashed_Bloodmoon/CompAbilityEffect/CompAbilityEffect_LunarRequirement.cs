79f06a3cd01a809c468335dfb241b43c1c4e4c2c
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-12-30 14:52:43 +1300

    moon veil ability

---
+++ 1.5/Source/Mashed_Bloodmoon/CompAbilityEffect/CompAbilityEffect_LunarRequirement.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class CompAbilityEffect_LunarRequirement : LycanthropeAbilityEffectComp
+    {
+        public new CompProperties_LunarRequirement Props => (CompProperties_LunarRequirement)props;
+
+        public override bool GizmoDisabled(out string reason)
+        {
+            if (!RequirementsMet())
+            {
+                reason = "Mashed_Bloodmoon_AbilityLunarRequirementNotMet".Translate();
+                return true;
+            }
+            return base.GizmoDisabled(out reason);
+        }
+
+        public bool RequirementsMet()
+        {
+            if (GenLocalDate.DayPercent(parent.pawn) < 0.25f || GenLocalDate.DayPercent(parent.pawn) > 0.8f)
+            {
+                return true;
+            }
+            foreach (GameConditionDef def in Props.gameConditions)
+            {
+                if (parent.pawn.Map.gameConditionManager.ConditionIsActive(def))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
