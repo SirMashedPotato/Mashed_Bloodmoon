f869be4d38d2f242d943cf815a680cddf273f3f5
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-02-18 11:57:48 +1300

    initial summon spectral werewolf ability

---
+++ 1.5/Source/Mashed_Bloodmoon/CompAbilityEffect/CompAbilityEffect_SummonBeast.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace Mashed_Bloodmoon
+{
+    public class CompAbilityEffect_SummonBeast : LycanthropeAbilityEffectComp
+    {
+        public new CompProperties_AbilitySummonBeast Props => (CompProperties_AbilitySummonBeast)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            for (int i = 0; i < Props.count; i++)
+            {
+                Pawn pawn = PawnGenerator.GeneratePawn(Props.pawnKindDef, null);
+                GenSpawn.Spawn(pawn, target.Cell, parent.pawn.MapHeld, WipeMode.Vanish);
+                AddLinkedHediff(pawn);
+                PlaySummonEffects(pawn);
+                StartMentalState(pawn);
+            }
+            base.Apply(target, dest);
+        }
+
+        private void AddLinkedHediff(Pawn pawn)
+        {
+            Hediff hediff = HediffMaker.MakeHediff(Props.linkedHediff, pawn);
+            HediffComp_Disappears hediffComp_Disappears = hediff.TryGetComp<HediffComp_Disappears>();
+            if (hediffComp_Disappears != null)
+            {
+                hediffComp_Disappears.ticksToDisappear = parent.def.GetStatValueAbstract(RimWorld.StatDefOf.Ability_Duration).SecondsToTicks();
+            }
+            pawn.health.AddHediff(hediff);
+            CompLycanthropeTransformed.linkedHediffs.Add(hediff);
+        }
+
+        private void PlaySummonEffects(Pawn pawn) 
+        {
+            SoundDefOf.Mashed_Bloodmoon_BeastHowl.PlayOneShot(new TargetInfo(pawn.Position, pawn.Map));
+            MoteMaker.MakeAttachedOverlay(pawn, ThingDefOf.Mashed_Bloodmoon_TransformEffect, Vector3.zero, pawn.DrawSize.y);
+        }
+
+        private void StartMentalState(Pawn pawn)
+        {
+            if (Props.stateDef != null)
+            {
+                pawn.mindState.mentalStateHandler.TryStartMentalState(Props.stateDef);
+            }
+        }
+    }
+}
---
+++ 1.5/Source/Mashed_Bloodmoon/CompAbilityEffect/Properties/CompProperties_AbilitySummonBeast.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class CompProperties_AbilitySummonBeast : CompProperties_AbilityEffect
+    {
+        public CompProperties_AbilitySummonBeast() => compClass = typeof(CompAbilityEffect_SummonBeast);
+
+        public PawnKindDef pawnKindDef;
+        public HediffDef linkedHediff;
+        public MentalStateDef stateDef;
+        public int count = 1;
+        public float radius = 1.9f;
+    }
+}
