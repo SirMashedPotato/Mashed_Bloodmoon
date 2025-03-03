e4afd0c739066b23ddf3ba03a51acb8b990da1ba
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-03-04 10:59:14 +1300

    bloodmoon incidents cycle implemented

---
+++ 1.5/Source/Mashed_Bloodmoon/GameCondition/GameCondition_HuntsmansMoon.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mashed_Bloodmoon
+{
+    public class GameCondition_HuntsmansMoon : GameCondition
+    {
+        public override void GameConditionTick()
+        {
+            base.GameConditionTick();
+        }
+    }
+}
---
+++ 1.5/Source/Mashed_Bloodmoon/StorytellerComp/StorytellerComp_CategoryMTBCustom.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class StorytellerComp_CategoryMTBCustom : StorytellerComp
+    {
+        public StorytellerCompProperties_CategoryMTBCustom Props => (StorytellerCompProperties_CategoryMTBCustom)props;
+
+        public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
+        {
+            float num = Props.mtbDays; //TODO switch to value in settings
+
+            if (Rand.MTBEventOccurs(num, GenDate.TicksPerDay, GenTicks.TickLongInterval))
+            {
+                IncidentParms parms = GenerateParms(Props.category, target);
+                if (TrySelectRandomIncident(UsableIncidentsInCategory(Props.category, parms), out var foundDef, target))
+                {
+                    yield return new FiringIncident(foundDef, this, parms);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} {Props.category}";
+        }
+    }
+}
