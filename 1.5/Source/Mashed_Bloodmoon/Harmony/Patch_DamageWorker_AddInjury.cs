7ccae8b9c3fd2e7423223870dceb4e6f3517536f
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-05-02 14:27:50 +1200

    Silver weakness redo
    - Silver weakness now handled through a harmony patch.
    - Silver weakness now increases damage taken, instead off adding an additional burn.
    - Updated relevant descriptions to reflect this.

---
+++ 1.5/Source/Mashed_Bloodmoon/Harmony/Patch_DamageWorker_AddInjury.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    /// <summary>
+    /// Increase damage taken if the source is made out of silver
+    /// </summary>
+    [HarmonyPatch(typeof(DamageWorker_AddInjury))]
+    [HarmonyPatch("ApplyToPawn")]
+    public static class DamageWorker_AddInjury_ApplyToPawn_Patch
+    {
+        public static void Prefix(DamageInfo dinfo, Pawn pawn)
+        {
+            float silverWeakness = pawn.GetStatValue(StatDefOf.Mashed_Bloodmoon_LycanthropeSilverWeakness);
+            if (silverWeakness > 0)
+            {
+                if (dinfo.Instigator is Pawn p)
+                {
+                    if (p.equipment != null && p.equipment.Primary != null)
+                    {
+                        Thing t = p.equipment.Primary;
+                        if (t.Stuff != null && t.Stuff == RimWorld.ThingDefOf.Silver
+                            || (t.def.costList != null && t.def.costList.Where(x => x.thingDef == RimWorld.ThingDefOf.Silver) != null))
+                        {
+                            dinfo.SetAmount(dinfo.Amount * (silverWeakness + 1f));
+                            Log.Message("modified = " + dinfo.Amount);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- 1.5/Source/Mashed_Bloodmoon/HediffComp/Properties/HediffCompProperties_SilverWeakness.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace Mashed_Bloodmoon
-{
-    public class HediffCompProperties_SilverWeakness : HediffCompProperties
-    {
-        public HediffCompProperties_SilverWeakness() => compClass = typeof(HediffComp_SilverWeakness);
-    }
-}
