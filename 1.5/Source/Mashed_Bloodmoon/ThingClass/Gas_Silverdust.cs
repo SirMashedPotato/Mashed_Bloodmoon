ae7d7f273907ae85a64888d3c08ebbb1fbfae5e6
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-05-06 14:18:52 +1200

    silverdust vessel belt

---
+++ 1.5/Source/Mashed_Bloodmoon/ThingClass/Gas_Silverdust.cs
@@ -0,0 +1,44 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class Gas_Silverdust : Gas
+    {
+        private readonly int tickInterval = 120;
+        private readonly float baseDamage = 6;
+
+        public override void Tick()
+        {
+            base.Tick();
+            try
+            {
+                if (this.IsHashIntervalTick(tickInterval))
+                {
+                    HashSet<Thing> hashSet = new HashSet<Thing>(Position.GetThingList(Map));
+                    if (hashSet != null)
+                    {
+                        foreach (Thing thing in hashSet)
+                        {
+                            if (thing != null && thing is Pawn p)
+                            {
+                                float silverWeakness = p.GetStatValue(StatDefOf.Mashed_Bloodmoon_LycanthropeSilverWeakness);
+                                if(silverWeakness > 0)
+                                {
+                                    DamageInfo dinfo = new DamageInfo(DamageDefOf.Burn, baseDamage * silverWeakness, 0, -1, null);
+                                    p.TakeDamage(dinfo);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+
+            }
+        }
+    }
+}
