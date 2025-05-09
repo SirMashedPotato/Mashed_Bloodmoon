1454857014977bc7aca884651e749ec485620a07
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-05-10 09:41:22 +1200

    houndslayer throwing axe

---
+++ 1.5/Source/Mashed_Bloodmoon/DamageWorker/DamageWorker_CutSilver.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class DamageWorker_CutSilver : DamageWorker_Cut
+    {
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+
+            float pawnSilverWeakness = thing.GetStatValue(StatDefOf.Mashed_Bloodmoon_LycanthropeSilverWeakness);
+            if (pawnSilverWeakness > 0)
+            {
+                float silverDamageFactor = dinfo.Weapon.GetStatValueAbstract(StatDefOf.Mashed_Bloodmoon_SilverDamageFactor);
+                dinfo.SetAmount(LycanthropeUtility.LycanthropeSilverDamageFactor(dinfo.Amount, pawnSilverWeakness, silverDamageFactor > 0 ? silverDamageFactor : 1f));
+            }
+
+            return base.Apply(dinfo, thing);
+        }
+    }
+}
