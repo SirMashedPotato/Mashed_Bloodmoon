bc2ae829623047e03792c74a8bdf281655672d05
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-05-10 16:29:49 +1200

    Adrenaline rush and huntsman's mark proficiency upgrades

---
+++ 1.5/Source/Mashed_Bloodmoon/HediffComp/HediffComp_ResetCooldownOnKilled.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    internal class HediffComp_ResetCooldownOnKilled : HediffComp
+    {
+        public HediffCompProperties_ResetCooldownOnKilled Props => (HediffCompProperties_ResetCooldownOnKilled)props;
+
+        public override void Notify_PawnKilled()
+        {
+            HediffComp_Link compLink = parent.TryGetComp<HediffComp_Link>();
+            compLink?.OtherPawn.abilities.GetAbility(Props.abilityDef);
+            base.Notify_PawnKilled();
+        }
+    }
+}
---
+++ 1.5/Source/Mashed_Bloodmoon/HediffComp/Properties/HediffCompProperties_ResetCooldownOnKilled.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class HediffCompProperties_ResetCooldownOnKilled : HediffCompProperties
+    {
+        public HediffCompProperties_ResetCooldownOnKilled() => compClass = typeof(HediffComp_ResetCooldownOnKilled);
+
+        public LycanthropeBeastHuntDef beastHuntDef;
+        public AbilityDef abilityDef;
+    }
+}
