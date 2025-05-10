2a06c199a66553abbad85816d7963070100e6644
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-05-10 18:07:42 +1200

    simple sidearms compat
    - Added a harmony patch to prevent equipping sidearms while in beast form.
    - Added a harmony patch to hide the sidearms gizmo, and should also prevent the use of sidearms, while in beast form.

--- 1.5/Source/Mashed_Bloodmoon/Harmony/HarmonyInstance.cs
+++ 1.5/Source/Mashed_Bloodmoon/Harmony/HarmonyInstance.cs
@@ -15,15 +15,21 @@ namespace Mashed_Bloodmoon
 
             harmony.Patch(AccessTools.Method(typeof(EquipmentUtility), nameof(EquipmentUtility.CanEquip), new[] { typeof(Thing), typeof(Pawn), typeof(string).MakeByRefType(), typeof(bool) }),
                 postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.EquipmentUtility_CanEquip_Patch)));
-        }
 
+            if (ModsConfig.IsActive("petetimessix.simplesidearms"))
+            {
+                harmony.Patch(AccessTools.Method(AccessTools.TypeByName("PeteTimesSix.SimpleSidearms.Utilities.StatCalculator"), "CanPickupSidearmType"), postfix: new HarmonyMethod(typeof(Compat_SimpleSidearms), nameof(Compat_SimpleSidearms.StatCalculator_CanPickupSidearmType_Patch)));
+                harmony.Patch(AccessTools.Method(AccessTools.TypeByName("PeteTimesSix.SimpleSidearms.Extensions"), "IsValidSidearmsCarrierRightNow"), postfix: new HarmonyMethod(typeof(Compat_SimpleSidearms), nameof(Compat_SimpleSidearms.Extensions_IsValidSidearmsCarrierRightNow_Patch)));
+            }
+        }
     }
+
     public static class HarmonyPatches
     {
         /// <summary>
         /// Prevents transformed lycanthropes from equipping weapons
         /// </summary>
-        public static void EquipmentUtility_CanEquip_Patch(Thing thing, Pawn pawn, ref string cantReason, ref bool __result)
+        public static void EquipmentUtility_CanEquip_Patch(Pawn pawn, ref string cantReason, ref bool __result)
         {
             if (__result && LycanthropeUtility.PawnIsTransformedLycanthrope(pawn))
             {
