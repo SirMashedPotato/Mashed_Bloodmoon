644093aada561e9a591236bc8b1a72a9d8dff0d2
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-06-15 18:28:19 +1200

    totem stat bonus edits
    The bonuses from fortitude, sight and tenacity are now multiplicative with other modifiers.

--- 1.5/Source/Mashed_Bloodmoon/StatPart/StatPart_LycanthropeTotems.cs
+++ 1.5/Source/Mashed_Bloodmoon/StatPart/StatPart_LycanthropeTotems.cs
@@ -9,7 +9,7 @@ namespace Mashed_Bloodmoon
         {
             if (req.Thing != null && req.Thing is Pawn pawn && totemTypeDef.TotemStatBonus(pawn, out float bonus))
             {
-                return "Mashed_Bloodmoon_TotemStatOffset".Translate(totemTypeDef.LabelCap) + bonus;
+                return "Mashed_Bloodmoon_TotemStatOffset".Translate(totemTypeDef.LabelCap) + bonus.ToStringByStyle(parentStat.toStringStyle);
             }
             return null;
         }
@@ -24,10 +24,18 @@ namespace Mashed_Bloodmoon
             {
                 if (totemTypeDef.TotemStatBonus(pawn, out float bonus))
                 {
-                    val += bonus;
+                    if (isFactor)
+                    {
+                        val *= (parentStat.defaultBaseValue + bonus);
+                    }
+                    else
+                    {
+                        val += bonus;
+                    }
                 }
             }
         }
         public LycanthropeTotemDef totemTypeDef;
+        public bool isFactor = false;
     }
 }
