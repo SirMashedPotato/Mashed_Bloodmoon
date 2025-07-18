570d9bc8d2dbb846923b90fa5a4448837a27767a
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-07-18 18:53:21 +1200

    Update to version 1.0.7/8

--- 1.6/Source/Mashed_Bloodmoon/HediffComp/HediffComp_SaniesLupinus.cs
+++ 1.6/Source/Mashed_Bloodmoon/HediffComp/HediffComp_SaniesLupinus.cs
@@ -11,8 +11,24 @@ namespace Mashed_Bloodmoon
         {
             if (parent.Severity + severityAdjustment >= 1.0f && !parent.FullyImmune())
             {
-                Messages.Message("Mashed_Bloodmoon_SaniesLupinusTransitioned".Translate(parent.pawn), parent.pawn, MessageTypeDefOf.ThreatSmall);
-                parent.pawn.health.AddHediff(HediffDefOf.Mashed_Bloodmoon_LycanthropeDormant);
+                if (LycanthropeUtility.PawnIsDormantLycanthrope(parent.pawn))
+                {
+                    TransformationUtility.ApplyImminentTransformation(parent.pawn, 300);
+                }
+                else if (LycanthropeUtility.PawnIsTransformedLycanthrope(parent.pawn))
+                {
+                    LycanthropeUtility.GetCompLycanthropeTransformed(parent.pawn).StartFury();
+                }
+                else if (LycanthropeUtility.PawnIsLycanthrope(parent.pawn))
+                {
+                    LycanthropeUtility.GetCompLycanthrope(parent.pawn).TransformPawn(true);
+                }
+                else
+                {
+                    Messages.Message("Mashed_Bloodmoon_SaniesLupinusTransitioned".Translate(parent.pawn), parent.pawn, MessageTypeDefOf.ThreatSmall);
+                    parent.pawn.health.AddHediff(HediffDefOf.Mashed_Bloodmoon_LycanthropeDormant);
+                }
+
                 parent.pawn.health.RemoveHediff(parent);
                 return;
             }
