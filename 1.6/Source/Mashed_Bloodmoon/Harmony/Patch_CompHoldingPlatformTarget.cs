8196913f7439b3040ad4409234946ca9646a495e
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-08-06 12:32:58 +1200

    CompHoldingPlatformTarget_CanBeCaptured_Patch

---
+++ 1.6/Source/Mashed_Bloodmoon/Harmony/Patch_CompHoldingPlatformTarget.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    [HarmonyPatch(typeof(CompHoldingPlatformTarget))]
+    [HarmonyPatch("CanBeCaptured", MethodType.Getter)]
+    public static class CompHoldingPlatformTarget_CanBeCaptured_Patch
+    {
+        public static void Postfix(ref CompHoldingPlatformTarget __instance, ref bool __result)
+        {
+            if (__result)
+            {
+                if (!(__instance.parent is Pawn pawn))
+                {
+                    return;
+                }
+
+                if (!pawn.def.tradeTags.NullOrEmpty() && pawn.def.tradeTags.Contains("Mashed_Bloodmoon_FeralLycanthrope"))
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
