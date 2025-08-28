f969238000d08486b9a0897b406f5453b3ffdf33
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-08-28 17:33:56 +1200

    pack command ability

---
+++ 1.6/Source/Mashed_Bloodmoon/CompAbilityEffect/CompAbilityEffect_AnimalBuff.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class CompAbilityEffect_AnimalBuff : CompAbilityEffect_AddLinkedHediff
+    {
+        public new CompProperties_AbilityAnimalBuff Props => (CompProperties_AbilityAnimalBuff)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            if (target.Pawn == null || !target.Pawn.NonHumanlikeOrWildMan())
+            {
+                return;
+            }
+
+            if ((target.Pawn.Faction != null && target.Pawn.Faction == Faction.OfPlayerSilentFail)
+                || (target.Pawn.InMentalState && target.Pawn.MentalState.def == MentalStateDefOf.Mashed_Bloodmoon_SpectralBeast))
+            {
+                base.Apply(target, dest);
+
+                if (Props.beastHuntDef != null)
+                {
+                    if (Props.beastHuntDef.Completed(parent.pawn))
+                    {
+                        Hediff hediff = target.Pawn.health.hediffSet.GetFirstHediffOfDef(Props.hediffDef);
+                        if (hediff != null)
+                        {
+                            hediff.Severity = Props.proficientSeverity;
+                        }
+                    }
+                    else
+                    {
+                        Props.beastHuntDef.ProgressBeastHunt(parent.pawn);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 1.6/Source/Mashed_Bloodmoon/CompAbilityEffect/Properties/CompProperties_AbilityAnimalBuff.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+
+namespace Mashed_Bloodmoon
+{
+    public class CompProperties_AbilityAnimalBuff : CompProperties_AbilityGiveHediff
+    {
+        public bool onlyTame = true;
+        public LycanthropeBeastHuntDef beastHuntDef;
+        public float proficientSeverity = 1f;
+    }
+}
