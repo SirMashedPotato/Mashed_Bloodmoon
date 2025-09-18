e0c18e75a57b7b6438b0d785ed2364f21992a247
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-09-19 10:46:55 +1200

    consume heart fail warning

--- 1.6/Source/Mashed_Bloodmoon/CompAbilityEffect/CompAbilityEffect_ConsumeHeart.cs
+++ 1.6/Source/Mashed_Bloodmoon/CompAbilityEffect/CompAbilityEffect_ConsumeHeart.cs
@@ -66,7 +66,7 @@ namespace Mashed_Bloodmoon
             }
             catch
             {
-
+                Log.Warning(parent.pawn + " tried to consume heart of " + target.Thing + " but it was already missing");
             }
             
             base.Apply(target, dest);
