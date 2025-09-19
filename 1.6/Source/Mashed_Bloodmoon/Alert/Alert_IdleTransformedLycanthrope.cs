2a925e6eab9dea3d6ab805945fbe974fbdc4c314
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-09-20 11:41:27 +1200

    idle transformed lycanthrope alert

---
+++ 1.6/Source/Mashed_Bloodmoon/Alert/Alert_IdleTransformedLycanthrope.cs
@@ -0,0 +1,53 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Text;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    public class Alert_IdleTransformedLycanthrope : Alert
+    {
+        private readonly List<Pawn> idleLycanthropeColonists = new List<Pawn>();
+        private readonly StringBuilder sb = new StringBuilder();
+
+        private List<Pawn> IdleLycanthropeColonists
+        {
+            get
+            {
+                idleLycanthropeColonists.Clear();
+                foreach (Pawn pawn in PawnsFinder.AllMaps_FreeColonistsAndPrisonersSpawned)
+                {
+                    if (LycanthropeUtility.PawnIsTransformedLycanthrope(pawn) && pawn.mindState.IsIdle)
+                    {
+                        idleLycanthropeColonists.Add(pawn);
+                    }
+                }
+                return idleLycanthropeColonists;
+            }
+        }
+
+        public override string GetLabel()
+        {
+            if (idleLycanthropeColonists.Count == 1)
+            {
+                return "Mashed_Bloodmoon_IdleTransformedLycanthrope_Label".Translate();
+            }
+            return "Mashed_Bloodmoon_IdleTransformedLycanthropes_Label".Translate(idleLycanthropeColonists.Count.ToStringCached());
+        }
+
+        public override TaggedString GetExplanation()
+        {
+            sb.Length = 0;
+            foreach (Pawn item in idleLycanthropeColonists)
+            {
+                sb.AppendLine("  - " + item.NameShortColored.Resolve());
+            }
+            return "Mashed_Bloodmoon_IdleTransformedLycanthrope_Desc".Translate(sb.ToString().TrimEndNewlines());
+        }
+
+        public override AlertReport GetReport()
+        {
+            return AlertReport.CulpritsAre(IdleLycanthropeColonists);
+        }
+    }
+}
