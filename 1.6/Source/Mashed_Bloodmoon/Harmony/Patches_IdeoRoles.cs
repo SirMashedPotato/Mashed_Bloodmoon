06d906f86b63c7a3982d73a5d7bdcd6aa2b48a18
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-10-27 18:46:20 +1300

    ideo role fix

---
+++ 1.6/Source/Mashed_Bloodmoon/Harmony/Patches_IdeoRoles.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Mashed_Bloodmoon
+{
+    /// <summary>
+    /// Prevents ideo roles with skill requirements from being removed when a lycanthrope enters beast form
+    /// Side effect that a lycanthrope in beast form can then be given any role with a skill requirement, even if they normally wouldn't meet it
+    /// </summary>
+    [HarmonyPatch(typeof(RoleRequirement_MinSkillAny))]
+    [HarmonyPatch("Met")]
+    public static class RoleRequirement_MinSkillAny_Met_Patch
+    {
+        public static void Postfix(Pawn p, ref bool __result)
+        {
+            if (!__result)
+            {
+                if (LycanthropeUtility.PawnIsTransformedLycanthrope(p))
+                {
+                    __result = true;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Prevents transformed lycanthropes being valid ritual targets for the role change ritual
+    /// </summary>
+    [HarmonyPatch(typeof(RitualRoleIdeoRoleChanger))]
+    [HarmonyPatch("AppliesToPawn")]
+    public static class RitualRoleIdeoRoleChanger_AppliesToPawn_Patch
+    {
+        public static void Postfix(Pawn p, ref string reason, ref bool __result)
+        {
+            if (__result)
+            {
+                if (LycanthropeUtility.PawnIsTransformedLycanthrope(p))
+                {
+                    __result = false;
+                    reason = "Mashed_Bloodmoon_InBeastForm".Translate(p);
+                }
+            }
+        }
+    }
+}
